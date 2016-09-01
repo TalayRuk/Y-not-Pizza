95ae4a3f28c67fe1b4c4d0d513df603e974d55c9
talayruk <notejde@gmail.com>
2016-08-31 17:51:02 -0700

    add need to fix warning

--- js/scripts.js
+++ js/scripts.js
@@ -57,21 +57,21 @@ Pizza.prototype.totalCost = function () {
 
 };
 
-  // function resetFields() {
-  //   $("input#new-first-name").val("");
-  //   $("input#new-last-name").val("");
-    // $("input.new-street").val("");
-    // $("input.new-city").val("");
-    // $("input.new-state").val("");
-    // $("input.new-zip").val("");
-    // $("input[name=specialty]").attr("checked", false);
-    // $("input[name=size]").attr("checked", false);
-    // $("input[name=sauce]").attr("checked", false);
-    // $("input[name=topping]").attr("checked", false);
-    // $("input[name=veggieToppings]").attr("checked", false);
-    // $("input.quantity").val("");
-    // $("input[name=carry-deliver]").attr("checked", false);
-  // }
+  function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("input.new-zip").val("");
+    $("input[name=specialty]").attr("checked", false);
+    $("input[name=size]").attr("checked", false);
+    $("input[name=sauce]").attr("checked", false);
+    $("input[name=topping]").attr("checked", false);
+    $("input[name=veggieToppings]").attr("checked", false);
+    $("input.quantity").val("");
+    $("input[name=carry-deliver]").attr("checked", false);
+  }
 // user logic
 $(function() {
   $("#hungry").click(function(){
@@ -100,11 +100,12 @@ $(function() {
 
 
     if(!newCustomer.checkCustomerName()) {
-      $("p .warning").text("DON'T FORGET TO ENTER YOUR NAME");
+      $("p #warning1").text("DON'T FORGET TO ENTER YOUR NAME");
+      console.log()
     }
 
     if(!newAddress.checkCustomerAddress()) {
-      $("p #warning").text("PLEASE ENTER CORRECT INFORMATION");
+      $("p #warning2").text("PLEASE ENTER CORRECT INFORMATION");
     }
 
     var inputSpecialty = $("input[name=specialty]:checked").length;
@@ -118,8 +119,6 @@ $(function() {
     $("ul#addresses").append("<li>" + newAddress.fullAddress() + "</li>")
     $(".total").text(addPizza.totalCost());
 
-      //   $("h4").show();
-      // });
-      // resetFields();
+      resetFields();
   });
 });
